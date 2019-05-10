bc3da78bf1fd53bb5976bf9a5c3723234f481bbc
abdulrahman118 <developer.abdulrahman@gmail.com>
2019-05-10 10:12:00 +0530

    fix homepage

--- src/components/Card.js
+++ src/components/Card.js
@@ -4,10 +4,10 @@ const Card = ({id,name,email}) => {
     return (
         <div className='tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5'>
             <img alt='robot' src={`https://robohash.org/${id}?size=200x200`} />
-        <div>
-            <h2>{name}</h2>
-            <p>{email}</p>
-        </div>
+            <div>
+                <h2>{name}</h2>
+                <p>{email}</p>
+            </div>
         </div>
 
     )
